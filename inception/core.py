e773c156cd0e16e77a398257b53cd6dbdc6ae368
harikrishnanm778 <harikrishnanm778@gmail.com>
2018-10-06 23:55:30 +0530

    U1:PB3 - change in combining directory and file path

--- inception/core.py
+++ inception/core.py
@@ -1,21 +1,24 @@
-# from collections import tuple
 import os
-Files = ""
 
 
+# Fetch files from the folder given as argument
 def FetchFilesInFolder(directory):
     if os.path.isdir(directory):
-        files = tuple(item for item in os.listdir(directory) if os.path.isfile(directory + '\\' + item))
+        files = tuple(item for item in os.listdir(directory)
+                      if os.path.isfile(os.path.join(directory, item)))
         return(files)
     else:
         return(None)
 
+# test code for getting files a directory using
+# FetchFilesInFolder() function and print them
 '''
+Files = ""
 testpath = input("enter path\n")
 Files = FetchFilesInFolder(testpath)
 if(Files):
     for File in Files:
         print(File)
 else:
-    print('folder doesnt exist')
-'''  
+    print('folder doesnt exist or it is empty')
+'''
