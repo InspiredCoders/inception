22e8b488ae46dc0a8909b028851550b32b1ec364
linson-93 <webclassrooms@gmail.com>
2018-10-08 22:06:04 +0530

    added function to get user input

--- inception/program.py
+++ inception/program.py
@@ -1,1 +1,14 @@
-#Assume Test code
+import os
+
+
+def get_user_input():
+    '''
+    Function to read user input
+    '''
+    path = ''
+    while True:
+        path = input('Provide valid folder path: ')
+        if not path.strip():
+            continue
+        break
+    return path
