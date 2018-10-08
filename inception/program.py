3c8ed6e39a0471cfa8f9941f2cc5ad15c1919c42
linson-93 <webclassrooms@gmail.com>
2018-10-08 22:14:00 +0530

    Added function to validate folder path

--- inception/program.py
+++ inception/program.py
@@ -8,7 +8,15 @@ def get_user_input():
     path = ''
     while True:
         path = input('Provide valid folder path: ')
-        if not path.strip():
+        if not path.strip() or not validate_path(path):
             continue
         break
     return path
+
+
+def validate_path(folder_path):
+    '''
+    Function to validate whether the path provided is a
+    correct folder path or not.
+    '''
+    return os.path.isdir(folder_path)
