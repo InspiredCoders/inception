8bd4a70e58d56d75830bca314c51748f164dfdeb
linson-93 <webclassrooms@gmail.com>
2018-10-09 22:30:22 +0530

    Added parameter to function

--- inception/config.py
+++ inception/config.py
@@ -1,12 +1,12 @@
 import json
 
 
-def read_config():
+def read_config(file_path='config.json'):
     '''
     Function to read configuration file and
     return configuration dictionary.
     '''
     config_data = {}
-    with open('config.json') as config_file:
+    with open(file_path) as config_file:
         config_data = json.load(config_file)
     return config_data
