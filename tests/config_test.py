b6dfa03c0e346b47d2ade4e126f7cf076d260222
linson-93 <webclassrooms@gmail.com>
2018-10-10 22:41:05 +0530

    Added test for function in config

--- tests/config_test.py
+++ tests/config_test.py
@@ -10,6 +10,20 @@ def test_type_read_config():
     config_file = os.path.join(os.path.dirname(__file__),
                                os.pardir, "inception",
                                "config.json")
-    print(config_file)
     result = config.read_config(config_file)
+    assert type(result) is dict
+
+
+def test_type_get_mapping_dictionary():
+    config_data = {
+        "Documents": "docx, doc, pdf, xls, xlsx",
+        "Music": "mp3, ogg, wav",
+        "Pictures": "jpg, jpeg, png, bmp",
+        "Videos": "mp4, mov, mkv",
+        "torrent_files": "torrent",
+        "linux_packages": "deb, rpm",
+        "compressed": "zip, rar, tar.gz, tar",
+        "Python Codes": "py, ipynb"
+    }
+    result = config.get_mapping_dictionary(config_data)
     assert type(result) is dict
