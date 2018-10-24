f156d5d213b7d13f33c72d64753412261c38f4ff
linson-93 <webclassrooms@gmail.com>
2018-10-24 20:50:44 +0530

    Changed gui to glade file

--- inception/window.py
+++ inception/window.py
@@ -3,47 +3,25 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 
-class MainWindow(Gtk.Window):
-
-    def __init__(self):
-        Gtk.Window.__init__(self, title="Inception")
-        self.set_border_width(30)
-        layout = Gtk.Box(spacing=8)
-        self.add(layout)
-
-        self.path = Gtk.Entry()
-        self.path.set_editable(False)
-        layout.add(self.path)
-        button = Gtk.Button('Browse')
-        button.connect('clicked', self.on_file_clicked)
-        self.button2 = Gtk.Button('Run')
-        self.button2.set_sensitive(False)
-        self.button2.connect('clicked', self.on_run_clicked)
-        layout.add(button)
-        layout.add(self.button2)
-
-    def on_run_clicked(self, widget):
-        pass
-
-    def on_file_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog("Please choose a folder", self,
-                                       Gtk.FileChooserAction.SELECT_FOLDER,
-                                       (Gtk.STOCK_CANCEL,
-                                        Gtk.ResponseType.CANCEL,
-                                        "Select", Gtk.ResponseType.OK))
-
-        dialog.set_default_size(400, 200)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            folder_path = dialog.get_filename()
-            self.path.set_text(str(folder_path))
-            self.button2.set_sensitive(True)
-
-        dialog.destroy()
+class WindowHandler:
+    def __init__(self, window):
+        self.win = window
 
 
-window = MainWindow()
-window.connect('delete-event', Gtk.main_quit)
-window.show_all()
-Gtk.main()
+class MainWindow:
+    def __init__(self, handler):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("window.glade")
+        self.builder.connect_signals(handler(self))
+
+        self.set_window("main_window")
+
+    def set_window(self, window):
+        self.window = self.builder.get_object(window)
+        self.window.show_all()
+
+    def main(self):
+        Gtk.main()
+
+app = MainWindow(WindowHandler)
+app.main()
