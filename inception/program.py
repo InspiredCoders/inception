a5ab26f4e4a27a1d72fb978d5bd75c8c768daa53
linson-93 <webclassrooms@gmail.com>
2018-10-24 22:44:36 +0530

    Removed cli code and changed functions

--- inception/program.py
+++ inception/program.py
@@ -4,36 +4,10 @@ from core import get_file_names, create_folders_if_necessary, move_files
 from config import read_config, get_mapping_dictionary
 
 
-def validate_path(folder_path):
-    '''
-    Function to validate whether the path provided is a
-    correct folder path or not.
-    '''
-    return os.path.isdir(folder_path)
-
-
-def get_cli_args():
-    '''
-    Function to collect the command line arguments from user.
-    '''
-    doc = """Inception
-
-            Usage:
-            program.py <folder_path>
-    """
-    return docopt(doc, version='Inception 1.0')
-
-
-def main():
-    arguments = get_cli_args()
-    location = arguments['<folder_path>']
+def run_app(location):
     configured_data = read_config()
     create_folders_if_necessary(
         list(configured_data.keys()), location
     )
     mapping_dictionary = get_mapping_dictionary(read_config())
     move_files(mapping_dictionary, location)
-
-
-if __name__ == '__main__':
-    main()
