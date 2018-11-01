6648eb2fabe4f012ae064d968257e94369b69df2
linson-93 <webclassrooms@gmail.com>
2018-11-01 19:02:31 +0530

    Scaffolding for application code

---
+++ inception/app.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QDialog
+from mainwindow import Ui_AppWindow
+from aboutwindow import Ui_AboutDialog
+from successdialog import Ui_SuccessDialog
+from warningdialog import Ui_WarningDialog
+
+
+class MainWindow(QMainWindow):
+    pass
+
+
+class AboutDialog(QDialog):
+    pass
+
+
+class SuccessDialog(QDialog):
+    pass
+
+
+class WarningDialog(QDialog):
+    pass
