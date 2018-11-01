d965aab0f3572d6d5eb27ef8d9f0f889706a061a
linson-93 <webclassrooms@gmail.com>
2018-11-01 19:55:17 +0530

    Added program logic

--- inception/app.py
+++ inception/app.py
@@ -5,19 +5,46 @@ from mainwindow import Ui_AppWindow
 from aboutwindow import Ui_AboutDialog
 from successdialog import Ui_SuccessDialog
 from warningdialog import Ui_WarningDialog
+from program import run_app
 
 
 class MainWindow(QMainWindow):
-    pass
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_AppWindow()
+        self.ui.setupUi(self)
+        self.ui.folder_select.clicked.connect(self.select_folder)
+        self.ui.file_group.clicked.connect(self.group_files)
+        self.ui.actionAbout.triggered.connect(self.show_about)
+
+    def select_folder(self):
+        path = str(QFileDialog.getExistingDirectory(self, "Select a folder"))
+        self.ui.folder_path.setText(path)
+
+    def show_dialog(self, dialog_window):
+        dialog = QDialog()
+        dialog.ui = dialog_window()
+        dialog.ui.setupUi(dialog)
+        dialog.exec_()
+        dialog.show()
+
+    def show_about(self):
+        self.show_dialog(Ui_AboutDialog)
+
+    def group_files(self):
+        path = self.ui.folder_path.text()
+
+        if path:
+            run_app(path)
+            self.ui.folder_path.setText("")
+            self.show_dialog(Ui_SuccessDialog)
+        else:
+            self.show_dialog(Ui_WarningDialog)
 
 
-class AboutDialog(QDialog):
-    pass
-
-
-class SuccessDialog(QDialog):
-    pass
-
-
-class WarningDialog(QDialog):
-    pass
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
--- inception/program.py
+++ inception/program.py
@@ -3,29 +3,7 @@ from core import get_file_names, create_folders_if_necessary, move_files
 from config import read_config, get_mapping_dictionary
 
 
-def get_user_input():
-    '''
-    Function to read user input
-    '''
-    path = ''
-    while True:
-        path = input('Provide valid folder path: ')
-        if not path.strip() or not validate_path(path):
-            continue
-        break
-    return path
-
-
-def validate_path(folder_path):
-    '''
-    Function to validate whether the path provided is a
-    correct folder path or not.
-    '''
-    return os.path.isdir(folder_path)
-
-
-def main():
-    location = get_user_input()
+def run_app(location):
     configured_data = read_config()
     create_folders_if_necessary(
         list(configured_data.keys()), location
